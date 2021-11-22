048ab39492c35df9e42f2464de401008ed34efd8
Yuvraj91 <seyuvraj290905@gmail.com>
2021-11-22 17:04:25 +0530

    default starting and ending node values

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+typedef struct node
+{
+    char previous_node_hash[258];
+    char node_hash[258];
+    char node_content[258];
+    char next_node[258];
+}
+node;
 
 int main(void){
-    printf("Hello World\n");
+    node temp;
+    char default_starting_node_hash[258] = 0x0;
+    char default_ending_node_hash[258] = 0xf;
+    
+
 }
